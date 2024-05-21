f64b099aab5744d9332aa0e3c3671865997752da
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-05-21 15:47:51 -0400

    :pray:

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,35 @@
 #include <Geode/modify/LevelInfoLayer.hpp>
-#include <math.h>
-#include <string>
-#include <fmt/format.h>
+#include <cmath>
 
 using namespace geode::prelude;
 
 std::string createXLString(int levelLengthMinutes) {
 	std::string XLlabel;
-	int numberOfXs = log2(levelLengthMinutes);
-	log::info("Number of Xs is {}", numberOfXs);
-	for (int i=0; i<numberOfXs & i<10; i++) {
+	for (int i = 0; i < log2(levelLengthMinutes) & i < 10; i++) {
 		XLlabel = XLlabel.append("X");
 	}
 	XLlabel = XLlabel.append("L");
 	return XLlabel;
 };
 
-class $modify(LevelInfoLayer) {
+class $modify(MyLevelInfoLayer, LevelInfoLayer) {
 
 	void createXLlabel() {
-		if (m_level->isPlatformer()) {
-			return;
+		if (!m_level->isPlatformer()) {
+			int levelLengthMinutes = m_level->m_timestamp / 14400;
+			if (levelLengthMinutes >= 4) {
+				m_lengthLabel->setString(createXLString(levelLengthMinutes).c_str());
+			}
 		}
-
-		int levelLengthMinutes = m_level->m_timestamp/14400;
-		log::info("length in minutes is {}", levelLengthMinutes);
-
-		if (levelLengthMinutes < 4) {
-			return;
-		}
-
-		std::string XLlabel = createXLString(levelLengthMinutes);
-		m_lengthLabel->setString(fmt::format("{}", XLlabel).c_str());
 	}
 
 	void setupLevelInfo() {
 		LevelInfoLayer::setupLevelInfo();
-		createXLlabel();
+		MyLevelInfoLayer::createXLlabel();
 	}
 
 	virtual void levelDownloadFinished(GJGameLevel* p0) {
 		LevelInfoLayer::levelDownloadFinished(p0);
-		createXLlabel();
+		MyLevelInfoLayer::createXLlabel();
 	}
 };
